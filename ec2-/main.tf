25e1bee4dd5ce8a8e513d8a9240e9c26aec85f4a
Ravindra <135192177+RavindraGhodi@users.noreply.github.com>
2023-07-16 20:37:25 +0530

    Update main.tf

--- ec2-/main.tf
+++ ec2-/main.tf
@@ -1,39 +1,32 @@
-
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.27"
     }
   }
+
+  required_version = ">= 0.14.9"
 }
 
 provider "aws" {
-  region = "us-west-2"  # Replace with your desired AWS region
+  profile = "default"
+  region  = "us-east-2"
 }
 
-resource "aws_instance" "ec2_instance" {
-  count         = 5
-  ami           = "ami-0c94855ba95c71c99"  # Replace with your desired AMI ID
+resource "aws_instance" "app_server" {
+  ami           = "ami-064ff912f78e3e561"
   instance_type = "t2.micro"
-  
-  # Replace with your desired key pair name
-  key_name      = "your-key-pair-name"
-  
-  # Replace with your desired security group IDs
-  security_group_ids = ["sg-0123456789abcdef0", "sg-0123456789abcdef1"]
 
   tags = {
-    Name = "EC2Instance-${count.index}"
+    Name = "Ravindra_ghodi2"
   }
 }
-
-
-Make sure to replace the following values with your own configurations:
-
-- Replace `"us-west-2"` with your desired AWS region.
-- Replace `"ami-0c94855ba95c71c99"` with the AMI ID you want to use.
-- Replace `"your-key-pair-name"` with the name of your key pair.
-- Replace `["sg-0123456789abcdef0", "sg-0123456789abcdef1"]` with the security group IDs you want to associate with the instances.
-
-Note: Ensure that you have the appropriate AWS credentials set up for Terraform to authenticate and access your AWS account.
+# to see public ip
+output "ec2_public_ips" {
+        value = aws_instance.app_server.public_ip
+}
+# to see private ip
+output "ec2_private_ips" {
+        value = aws_instance.app_server.private_ip
+}
